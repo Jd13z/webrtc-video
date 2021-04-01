2ab2ae26507e77ef1e0f3b88b3d849bcd038daca
Josué Aquise <josue.aquise@outlook.com>
2021-04-01 17:52:16 -0500

    react app webRTC

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home';
+import VideoCatch from './components/VideoCatch';
+import VideoRecordComp from './components/VideoRecordComp';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/:id" component={VideoRecordComp} exact />
+        <Route path="/catch/:id" component={VideoCatch} />
+      </Switch>
+      </BrowserRouter>
+      
     </div>
   );
 }
